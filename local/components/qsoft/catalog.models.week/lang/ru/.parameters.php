c4f2304266c55c9bd84b0cfd06c29bbcf170aadb
PavLikh <59540515+PavLikh@users.noreply.github.com>
2020-07-08 21:58:07 +0300

    Create .parameters.php

---
+++ local/components/qsoft/catalog.models.week/lang/ru/.parameters.php
@@ -0,0 +1,16 @@
+<?
+$MESS["IBLOCK_DETAIL_URL"] = "URL, ведущий на страницу с содержимым элемента раздела";
+$MESS["IBLOCK_TYPE"] = "Тип инфоблока";
+$MESS["IBLOCK_IBLOCK"] = "Инфоблок";
+$MESS["IBLOCK_ANY"] = "(любой)";
+$MESS["IBLOCK_SECTION_ID"] = "ID раздела";
+$MESS["CP_BPR_CACHE_GROUPS"] = "Учитывать права доступа";
+$MESS["ELEMENTS_QUANTITY"] = "Количество отображаемых элементов";
+$MESS["SECTION_URL"] = 'URL-адрес ссылки "ВСЕ"';
+$MESS["SORT_FIELD"] = 'Поле сортировки';
+$MESS["ORDER"] = 'Порядок сортировки';
+$MESS["ORDER_DESC"] = 'По убыванию';
+$MESS["SORT_RAND"] = 'Случайный порядок';
+
+
+?>
