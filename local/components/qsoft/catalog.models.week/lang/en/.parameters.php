2a83c8d16169b97ea9e2fefa702e072c117ad6f3
PavLikh <59540515+PavLikh@users.noreply.github.com>
2020-07-08 21:54:49 +0300

    Create .parameters.php

---
+++ local/components/qsoft/catalog.models.week/lang/en/.parameters.php
@@ -0,0 +1,15 @@
+<?
+$MESS["IBLOCK_DETAIL_URL"] = "URL of the page with the detail contents";
+$MESS["IBLOCK_TYPE"] = "Type of information block";
+$MESS["IBLOCK_IBLOCK"] = "Information block";
+$MESS["IBLOCK_ANY"] = "(any)";
+$MESS["IBLOCK_SECTION_ID"] = "Section ID";
+$MESS["CP_BPR_CACHE_GROUPS"] = "Respect Access Permissions";
+$MESS["ELEMENTS_QUANTITY"] = "Quantity of elements on page";
+$MESS["SECTION_URL"] = 'URL of the section';
+$MESS["SORT_FIELD"] = 'Sort field';
+$MESS["ORDER"] = 'Order';
+$MESS["ORDER_DESC"] = 'Descending';
+$MESS["SORT_RAND"] = 'Random';
+
+?>
