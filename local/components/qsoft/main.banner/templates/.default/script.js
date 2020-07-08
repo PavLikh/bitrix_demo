a8615f278ae627335447889ed101bc35b02c9ff3
PavLikh <59540515+PavLikh@users.noreply.github.com>
2020-07-08 22:09:23 +0300

    Create script.js

---
+++ local/components/qsoft/main.banner/templates/.default/script.js
@@ -0,0 +1,11 @@
+$(function(){
+   if ($(".bxslider").length > 0 && $(".bxslider .banner").length > 1) {
+       var slider = $(".bxslider").bxSlider({
+           auto: true,
+           pause: 3000,
+           onSlideAfter: function() {
+               slider.startAuto();
+           }
+       });
+   }
+});
